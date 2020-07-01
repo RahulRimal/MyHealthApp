e75d5e7ded672a17e5cfb4a810c1c60195d614df
Rahul Rimal <genuinetechnology9@gmail.com>
2020-07-01 14:18:44 +0545

    Added assets

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,6 +9,11 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+        ),
+      ),
 
     );
   }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -7,10 +7,42 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   @override
+  Widget get _space {
+    return SizedBox(height: 3.0);
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pocket Doctor'),
+      body: Column(
+        children: <Widget>[
+          Image.asset('assets/images/topIcon.png',
+          height: 100.0,),
+          Padding(
+            padding: const EdgeInsets.only(top: 150.0, left: 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    fontFamily: 'RobotoSlab',
+                    fontSize: 70.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                _space,
+                Text(
+                  'Please sign in to continue.',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontFamily: 'RobotoSlab',
+                    color: Colors.black45,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
